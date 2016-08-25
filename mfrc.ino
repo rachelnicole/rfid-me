72a23cdf6c2b3b74365d21dd8bb5670962d1a650
Rachel White <loveless@gmail.com>
2016-08-25 16:46:35 -0400

    Json type and serial. Add padding

--- mfrc.ino
+++ mfrc.ino
@@ -28,7 +28,7 @@ void setup() {
 void loop() {
   byte status;
   byte data[MAX_LEN];
-  byte serial[5];
+  byte serial[16];
   int i, j, pos;
 
   // Send a general request out into the aether. If there is a tag in
@@ -36,24 +36,25 @@ void loop() {
   status = nfc.requestTag(MF1_REQIDL, data);
 
   if (status == MI_OK) {
-    Serial.println("------------------------------");
-    Serial.println("Tag detected.");
-    Serial.print("Type: ");
-    Serial.print(data[0]);
-    Serial.print(", ");
-    Serial.println(data[1]);
+    Serial.println("[\"type\", " + String(data[0]) +  ", " + String(data[1]) + "]");
+
+    for (j = 0; j < 16; j++) {
+      data[j] = 127;
+    }
 
     // calculate the anti-collision value for the currently detected
     // tag and write the serial into the data array.
     status = nfc.antiCollision(data);
-    memcpy(serial, data, 5);
+    memcpy(serial, data, 16);
 
-    Serial.println("The serial nb of the tag is:");
-    for (i = 0; i < 3; i++) {
-      Serial.print(serial[i]);
-      Serial.print(", ");
+    Serial.print("[\"serial\", ");
+    for (i = 0; i < 16; i++) {
+      if (i > 0) {
+        Serial.print(", ");
+      }
+      Serial.print(String(serial[i]));
     }
-    Serial.println(serial[3]);
+    Serial.println("]");
 
     // Select the tag that we want to talk to. If we don't do this the
     // chip does not know which tag it should talk if there should be
