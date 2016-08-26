a572ed398a0ed09e0a678d2d7cd615b11a3ab945
Rachel White <loveless@gmail.com>
2016-08-25 21:34:29 -0400

    Javascript interface that receives rfid data

--- index.js
+++ index.js
@@ -10,10 +10,46 @@ port.on('open', function() {
   console.log('Opened port');
   port.on('data', function(text) {
     var data = JSON.parse(text);
-    process.stdout.write(data + '\n');
+    var type = data[0];
+    switch (type) {
+      case 'init':
+        rfidInitialized();
+        break;
+      case 'found':
+        rfidFound(data[1].toString(16));
+        break;
+      case 'type':
+        rfidType(data[2].toString(16) + data[1].toString(16));
+        break;
+      case 'anticol':
+        rfidAntiCollision(data[1].toString(16));
+        break;
+      case 'serial':
+        rfidSerial(data[2].toString(16) + ':' + data[3].toString(16) + ':' + data[4].toString(16));
+        break;
+    }
   });
   port.on('error', function(err) {
     console.log('Error: ', err.message);
   });
-})
+});
 
+function rfidInitialized() {
+  console.log('initialized');
+}
+
+function rfidFound(version) {
+  console.log('found firmware ' + version);
+}
+
+function rfidType(type) {
+  console.log('tag type ' + type);
+}
+
+function rfidAntiCollision(status) {
+  console.log('anti collision ' + status);
+}
+
+function rfidSerial(serial) {
+  console.log('serial ' + serial);
+}
